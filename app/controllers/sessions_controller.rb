0ca4511f55fbc78480c9e5ac508976fe53ce3c19
Dwayne McFarlane <dwaynemcfarlaneny@gmail.com>
2019-12-08 15:07:55 -0500

    added sessions and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
 
+    post '/login' do
+     @student = Student.find_by(email: params[:email])
+     if @student.authenticate(params[:password])
+        session[:student_id] = @student.id
+        redirect "/students/#{@student.id}"
+     end
+    end
     get '/signup' do
-        
+        erb :signup
     end
 
 end
